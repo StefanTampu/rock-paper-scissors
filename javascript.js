b5c58ef7604c6921dd548047a251f79117888c8a
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-03-25 10:47:56 -0400

    Create score variables and increase score through playRound()

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
- function getComputerChoice(){
+function getComputerChoice(){
     let num = Math.ceil(Math.random()*3);
     if (num === 1){
         return "Rock";
@@ -11,30 +11,43 @@
 
  console.log(getComputerChoice());
 
- function playRound(playerSelection, computerSelection){
-    let playerCap = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection){
+    let playerCap = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase(); 
     if (playerCap === computerSelection) {
         return "Tie!";
     } else if (playerCap == "Rock"){
         if (computerSelection == "Paper"){
-            return "You lose! Rock is beaten by paper.";
+            console.log("You lose! Rock is beaten by paper.");
+            return computerScore++;
         } else if (computerSelection == "Scissors"){
-            return "You win! Rock beats scissors.";
+            console.log("You win! Rock beats scissors.");
+            return playerScore++;
         }
     } else if (playerCap == "Paper"){
         if (computerSelection == "Rock"){
-            return "You win! Paper beats rock."
+            console.log("You win! Paper beats rock.");
+            return playerScore++;
         } else if (computerSelection == "Scissors"){
-            return "You lose! Paper is beaten by scissors."
+            console.log("You lose! Paper is beaten by scissors.");
+            return computerScore++;
         }
     }  else if (playerCap == "Scissors"){
         if (computerSelection == "Rock"){
-            return "You lose! Scissors are beaten by rock."
+            console.log("You lose! Scissors are beaten by rock.");
+            return computerScore++;
         } else if (computerSelection == "Paper"){
-            return "You win! Scissors beat rock."
+            console.log("You win! Scissors beat rock.");
+            return playerScore++;
         }
     }
  }
 
-const computerSelection = getComputerChoice();
-console.log(playRound("rOcK",computerSelection));
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
+
+
+console.log(playerScore);
+console.log(computerScore);
