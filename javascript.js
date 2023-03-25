f3b2669e7185c4b1c7d361c274cdaa382cc1874f
Stefan Tampu <114590807+StefanTampu@users.noreply.github.com>
2023-03-25 09:38:21 -0400

    Create playRound function
    This function compares the player and computer's selection, then returns a winner. It also makes the player's input case insensitive.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,28 @@
  }
 
  console.log(getComputerChoice());
+
+ function playRound(playerSelection, computerSelection){
+    let playerCap = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    if (playerCap === computerSelection) {
+        return "Tie!";
+    } else if (playerCap == "Rock"){
+        if (computerSelection == "Paper"){
+            return "You lose! Rock is beaten by paper.";
+        } else if (computerSelection == "Scissors"){
+            return "You win! Rock beats scissors.";
+        }
+    } else if (playerCap == "Paper"){
+        if (computerSelection == "Rock"){
+            return "You win! Paper beats rock."
+        } else if (computerSelection == "Scissors"){
+            return "You lose! Paper is beaten by scissors."
+        }
+    }  else if (playerCap == "Scissors"){
+        if (computerSelection == "Rock"){
+            return "You lose! Scissors are beaten by rock."
+        } else if (computerSelection == "Paper"){
+            return "You win! Scissors beat rock."
+        }
+    }
+ }
